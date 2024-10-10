28fc154443ac3d62d50fc857347098fbb4f0609a
kovancilartr <kovancilartr@gmail.com>
2024-10-10 21:44:21 +0300

    İlk2

--- components/custom/Navbar/MobileMenu.tsx
+++ components/custom/Navbar/MobileMenu.tsx
@@ -1,20 +1,20 @@
-import React from 'react'
-import NavItem from './NavItem';
+import React from "react";
+import NavItem from "./NavItem";
 
 interface MobileMenuProps {
-    links: [];
+  links: [];
 }
 
 const MobileMenu = ({ links }: MobileMenuProps) => {
   return (
-    <ul className='flex flex-col p-4 items-center'>
-      {links.map((link, index) => (
+    <ul className="flex flex-col p-4 items-center">
+      {links.map((link: { path: string; name: string }, index: number) => (
         <li key={index}>
           <NavItem href={link.path} title={link.name} />
         </li>
       ))}
     </ul>
-  )
-}
+  );
+};
 
-export default MobileMenu
+export default MobileMenu;
